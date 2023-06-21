ae0a867d94bf899246e0cb28f7f71bd7ce4d9de3
chrisKodehode <chrisa.kodehode@gmail.com>
2023-06-21 10:00:20 +0200

    Refactor login page and CSS styles for sign-up form
    - Adding a sign-up button with new CSS styles
    - Adding password and email fields to the form with new CSS styles
    - Updating the title of the page to "SIGN UP"
    - Removing unused code from login.css file

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,12 +1,56 @@
-import "./css/login.css"
+// import { useCallback } from "react";
+import { TextField } from "@mui/material";
+// import { useNavigate } from "react-router-dom";
+import "./css/login.css";
+const LoginPage = () => {
 
-function LoginPage() {
-    return (
-        <div>
-            <h1 className="SignUp-text">SIGN UP</h1>
+  return (
+    <div className="sign-up-page">
+      <form className="sign-up-form" id="signUp-form">
+
+        <button
+          className="sign-up-btn"
+          id="sign-up-btn"
+        >
+          <div className="sign-up">SIGN UP</div>
+        </button>
+
+        <div className="password-field">
+          <div className="password">Password</div>
+          <TextField
+            className="password-input-field"
+            sx={{ width: 488 }}
+            color="primary"
+            variant="outlined"
+            type="password"
+            name="Password"
+            id="password-input"
+            size="medium"
+            margin="none"
+            required
+          />
         </div>
-    )
-}
 
+        <div className="email-field">
+          <div className="email-title">Email</div>
+          <TextField
+            className="password-input-field"
+            sx={{ width: 488 }}
+            color="primary"
+            variant="outlined"
+            type="email"
+            name="email-text"
+            id="email-input"
+            size="medium"
+            margin="none"
+            required
+          />
+        </div>
+
+        <div className="sign-up1">SIGN UP</div>
+      </form>
+    </div>
+  );
+};
 
 export default LoginPage;
