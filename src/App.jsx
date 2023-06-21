e5cc848f1cd2409e788ebc86eb3689dbddd20c9e
chrisKodehode <chrisa.kodehode@gmail.com>
2023-06-21 09:14:51 +0200

    Add login, success and error pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import LoginPage from "./components/LoginPage"
+import SuccessPage from "./components/SuccessPage"
+import ErrorPage from "./components/ErrorPage"
+
 function App() {
-  
+  return (
+    <div>
+      <LoginPage />
+      <SuccessPage />
+      <ErrorPage />
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/SuccessPage.jsx
@@ -0,0 +1,9 @@
+function SuccessPage() {
+    return (
+        <div>
+        
+        </div>
+    )
+}
+
+export default SuccessPage;
